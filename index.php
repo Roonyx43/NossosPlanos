ff045805f826b9f38838e9d1f4fc5133fa94a5e7
Matheus Loch <122698448+Roonyx43@users.noreply.github.com>
2025-03-03 23:27:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -26,8 +26,8 @@
     <button class="valuesModal" onclick="abrirModal()">Sejam bem vindos!</button>
     <div class="overlay" id="overlay"></div>
     <div id="valuesModal" class="values-modal">
-        <h2>Olá familia linda!</h2>
-        <p>Este site é mais do que um simples espaço nosso, é um registro da nossa jornada, que esta sendo incrível para mim, saiba que eu te amo muito, e vou continuar te amando cada dia mais! E para nossos filhos que um dia verão isso, saibam: cada momento aqui representa o esforço, o amor e a dedicação que colocamos para construir um futuro melhor para vocês. Seu pai sempre se esforçou muito, e vai continuar se esforçando, para que vocês tenham uma vida cheia de felicidade. <br><br><h3>PS</h3><p>Respeitem a mamãe! Ela é brava!</p>
+        <h2>Olá família linda!</h2>
+        <p>Este site é mais do que um simples espaço nosso, é um registro da nossa jornada, que esta sendo incrível para mim, saiba que eu te amo muito, e vou continuar te amando cada dia mais! E para nossos filhos que um dia verão isso, saibam: cada momento aqui representa o esforço, o amor e a dedicação que colocamos para construir um futuro melhor para vocês. Seu pai e sua mãe sempre se esforçaram muito, e vão continuar se esforçando, para que vocês tenham uma vida cheia de felicidade. <br><br><h3>PS</h3><p>Respeitem a mamãe! Ela é brava!</p>
         </p>
         <button onclick="fecharModal()" class="closeModal">Fechar</button>
     </div>
